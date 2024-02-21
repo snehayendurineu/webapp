690fbbb8f0d5ecc61d545d70b686fdfb57698a51
snehayendurineu <yenduri.s@northeastern.edu>
2024-02-21 11:59:24 -0500

    editing yml files-20

--- packer/mysql-script.sh
+++ packer/mysql-script.sh
@@ -9,3 +9,5 @@ sudo systemctl start mysqld
 sudo systemctl enable mysqld
 
 sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Lakki0811';"
+
+sudo mysql --user="${{ secrets.DB_USER }}" --password="${{ secrets.DB_PASSWORD}}" -e "CREATE DATABASE IF NOT EXISTS ${{ secrets.DATABASE }};"
