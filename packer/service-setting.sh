172449b7748fd8a8d1bf3aea612d47c3a77d2ae3
snehayendurineu <yenduri.s@northeastern.edu>
2024-02-21 00:21:08 -0500

    editing yml files-1

--- packer/service-setting.sh
+++ packer/service-setting.sh
@@ -5,3 +5,4 @@ sudo mv /tmp/webapp.service /etc/systemd/system/
 cd /etc/systemd/system || exit 1
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
