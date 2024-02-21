2994ea68ac622b1968828a23f8099f962aeec9f7
snehayendurineu <yenduri.s@northeastern.edu>
2024-02-21 09:04:28 -0500

    editing yml files-14

--- packer/setup-script.sh
+++ packer/setup-script.sh
@@ -7,12 +7,12 @@ sudo dnf module enable nodejs:20
 sudo dnf module install -y nodejs:20
 
 
-echo "Unzip webapp"
+echo "Unzip webApp"
 ls
 cd /home/packer || exit 1
 sudo yum install -y unzip
-sudo unzip webapp.zip
-cd webapp || exit 1
+sudo unzip webApp.zip
+cd webApp || exit 1
 
 echo "installing npm"
 node --version
