5c47f6ffcf192e28cc21d391e81d7fae039dca16
snehayendurineu <yenduri.s@northeastern.edu>
2024-02-21 10:34:38 -0500

    editing yml files-19

--- packer/add-local-user.sh
+++ packer/add-local-user.sh
@@ -2,4 +2,6 @@
 
 sudo groupadd -f csye6225
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225 || exit 1 
-sudo chown -R csye6225:csye6225 webApp || exit 1 
+sudo chmod -R 755 /home/packer
+sudo chown -R csye6225:csye6225 /home/packer || exit 1 
+
