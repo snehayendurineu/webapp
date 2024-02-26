76f203e79bcf8ef64e431c91be948aef55138c13
snehayendurineu <yenduri.s@northeastern.edu>
2024-02-26 18:03:58 -0500

    removing mysql installation

--- packer/mysql-script.sh
+++ packer/mysql-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo yum install -y mysql-server
+#sudo yum install -y mysql-server
 
-sudo systemctl status mysqld
+#sudo systemctl status mysqld
 
-sudo systemctl start mysqld
+#sudo systemctl start mysqld
 
-sudo systemctl enable mysqld
+#sudo systemctl enable mysqld
 
-sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Lakki0811';"
+#sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Lakki0811';"
 
-sudo mysql --user="root" --password="Lakki0811" -e "CREATE DATABASE IF NOT EXISTS cloudDB;"
+#sudo mysql --user="root" --password="Lakki0811" -e "CREATE DATABASE IF NOT EXISTS cloudDB;"
