8c9b164bebcbcbae7f9dca3dca86859702a7841f
snehayendurineu <yenduri.s@northeastern.edu>
2024-03-15 22:05:43 -0400

    error corrected

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const routes = require('./routes/userRouter');
-
+const logger = require('./loggerModel.js');
 const app = express()
 
+logger.info("Executing app.js")
 if (require.main === module) {
     const port = 8080;
 
     app.listen(port, function(){
         console.log(`Server is running on port ${port}`);
+        logger.warn('Server is running on port 8080');
     });
 }
 
--- packer/restart-ops-agent.sh
+++ packer/restart-ops-agent.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
+sudo mkdir -p etc/google-cloud-ops-agent
 sudo mv /tmp/config.yml etc/google-cloud-ops-agent/config.yml
 
-sudo vi etc/google-cloud-ops-agent/config.yml
 sudo systemctl restart google-cloud-ops-agent
