c3e0e82c7e8018ab737429e6a0b8738e3bb3e374
snehayendurineu <yenduri.s@northeastern.edu>
2024-03-16 22:17:27 -0400

    changing permissions

--- packer/ops-agent.sh
+++ packer/ops-agent.sh
@@ -2,3 +2,5 @@
 
 sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+sudo mkdir -p /var/log/webapp/
+sudo chmod -R 755 /var/log/webapp/
