0fd3aab8813003906d3129317b49ea99010bf71a
snehayendurineu <yenduri.s@northeastern.edu>
2024-03-17 01:05:19 -0400

    changes

--- packer/restart-ops-agent.sh
+++ packer/restart-ops-agent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo mkdir -p etc/google-cloud-ops-agent
-sudo mv /tmp/config.yaml etc/google-cloud-ops-agent/config.yaml
+sudo cp /tmp/config.yaml etc/google-cloud-ops-agent/config.yaml
 
 sudo systemctl restart google-cloud-ops-agent
