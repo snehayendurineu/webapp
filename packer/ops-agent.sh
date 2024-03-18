ef1cb6f4d4a67fd975890a237e4d2e7edac4b8f8
snehayendurineu <yenduri.s@northeastern.edu>
2024-03-17 20:22:29 -0400

    crcted changes

--- packer/ops-agent.sh
+++ packer/ops-agent.sh
@@ -2,6 +2,8 @@
 
 sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
 sudo mkdir -p /var/log/webapp/
+sudo chown -R snehayenduri /var/log/webapp/ || exit 1 
 sudo chmod -R 755 /var/log/webapp/
 sudo touch /var/log/webapp/app.log
--- packer/restart-ops-agent.sh
+++ packer/restart-ops-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
+sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
