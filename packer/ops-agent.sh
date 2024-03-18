20e2fde0cae5e57187d8a284db55e36114f39fd9
snehayendurineu <yenduri.s@northeastern.edu>
2024-03-17 20:40:34 -0400

    issue solved

--- packer/ops-agent.sh
+++ packer/ops-agent.sh
@@ -4,6 +4,7 @@ sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 sudo mkdir -p /var/log/webapp/
-sudo chown -R snehayenduri /var/log/webapp/ || exit 1 
+sudo su root
+#sudo chown -R snehayenduri /var/log/webapp/ || exit 1 
 sudo chmod -R 755 /var/log/webapp/
 sudo touch /var/log/webapp/app.log
