c25832a196de76a078e6d2b1b42090b1985cf2db
snehayendurineu <yenduri.s@northeastern.edu>
2024-03-26 15:01:53 -0400

    edited files

--- app.test.js
+++ app.test.js
@@ -21,7 +21,7 @@ afterAll(async () => {
 
 describe('Integration tests', ()=>{
     const createUsr = {
-        "username": "snehayenduri08@gmail.com",
+        "username": "test@gmail.com",
         "password": "test123",
         "first_name": "test_fn",
         "last_name": "test_ln"
@@ -31,9 +31,9 @@ describe('Integration tests', ()=>{
            
             await supertest(app).post("/v1/user").send(createUsr).expect(201);
 
-            await User.update({ is_verified: true }, { where: { username: "snehayenduri08@gmail.com"} });
+            await User.update({ is_verified: true }, { where: { username: "test@gmail.com"} });
 
-            const getres = await supertest(app).get("/v1/user/self").set('Authorization', `Basic ${Buffer.from('snehayenduri08@gmail.com:test123').toString('base64')}`).expect(200);
+            const getres = await supertest(app).get("/v1/user/self").set('Authorization', `Basic ${Buffer.from('test@gmail.com:test123').toString('base64')}`).expect(200);
 
             const getUsr = getres.body;
 
@@ -52,16 +52,16 @@ describe('Integration tests', ()=>{
                 "last_name": "test_ln"
             }
 
-            await supertest(app).put(`/v1/user/self`).set('Authorization', `Basic ${Buffer.from('snehayenduri08@gmail.com:test123').toString('base64')}`).send(updateUsr).expect(204);
+            await supertest(app).put(`/v1/user/self`).set('Authorization', `Basic ${Buffer.from('test@gmail.com:test123').toString('base64')}`).send(updateUsr).expect(204);
 
-            const getres = await supertest(app).get("/v1/user/self").set('Authorization', `Basic ${Buffer.from('snehayenduri08@gmail.com:newtest123').toString('base64')}`).expect(200);
+            const getres = await supertest(app).get("/v1/user/self").set('Authorization', `Basic ${Buffer.from('test@gmail.com:newtest123').toString('base64')}`).expect(200);
 
             const getUsr = getres.body;
 
             assert.strictEqual(getUsr.first_name, updateUsr.first_name);
             assert.strictEqual(getUsr.last_name, updateUsr.last_name);
 
-            await supertest(app).delete("/v1/user/self").set('Authorization', `Basic ${Buffer.from('snehayenduri08@gmail.com:newtest123').toString('base64')}`).expect(200);
+            await supertest(app).delete("/v1/user/self").set('Authorization', `Basic ${Buffer.from('test@gmail.com:newtest123').toString('base64')}`).expect(200);
 
             
         })
